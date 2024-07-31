4fbc592b1036e28e6b13ea69b28faab48f4aea0b
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-01 01:22:11 +0600

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456789341246845
+
+// Reference (Non primitive)
+
+// Array, objects, Functions
+
+const heros = ["Superman", "Spidermen", "ddh"];
+let myObj = {
+    name: "Mehedi",
+    age: 29,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+
