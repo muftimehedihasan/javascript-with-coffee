c603fcedd019ccaa66354a4dcf6f0279fb10971c
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-01 16:05:59 +0600

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// singletone
+// Object.create
+
+// object literals
+
+const jsUser = {
+    name: "mehedi hasan",
+    eamil: "mehedi@gmail.com",
+    age: 29,
+    isLoggedin: true,
+    lastLogginDays: ["monday", "Saterday"]
+    
+}
+
+console.log(jsUser)
+console.log(jsUser.age)
+console.log(jsUser["eamil"])
