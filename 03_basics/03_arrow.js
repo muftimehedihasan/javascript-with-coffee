5494e5033852241f1fb5556028c056d4666f6436
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-08-02 01:38:21 +0600

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,13 @@
+// Traditional function expression
+const add1 = function(a, b) {
+    return a + b;
+  };
+  
+  // Arrow function expression
+  const add2 = (a, b) => {
+    return a + b;
+  };
+  
+  // Even shorter version (when there's only a single expression)
+  const add3 = (a, b) => a + b;
+  
